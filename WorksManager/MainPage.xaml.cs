747aede991abe1d8d6306c311c8212581ccf9b5c
muthu pandi <muthupandimp@outlook.com>
2021-06-07 17:44:04 +0530

    initial commit

---
+++ WorksManager/MainPage.xaml.cs
@@ -0,0 +1,113 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.ApplicationModel;
+using System;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.AppService;
+using Windows.UI.Core;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WorksManager
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                App.AppServiceConnected += MainPage_AppServiceConnected;
+                App.AppServiceDisconnected += MainPage_AppServiceDisconnected;
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            //{
+            //    await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            //}
+            //Frame.Navigate(typeof(StartPage), null);
+        }
+
+        private async void MainPage_AppServiceConnected(object sender, AppServiceTriggerDetails e)
+        {
+            App.Connection.RequestReceived += AppServiceConnection_RequestReceived;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // enable UI to access  the connection
+                //btnRegKey.IsEnabled = true;
+            });
+        }
+
+        private async void MainPage_AppServiceDisconnected(object sender, EventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // disable UI to access the connection
+                //btnRegKey.IsEnabled = false;
+
+                // ask user if they want to reconnect
+                Reconnect();
+            });
+        }
+
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            double d1 = (double)args.Request.Message["D1"];
+            double d2 = (double)args.Request.Message["D2"];
+            double result = d1 + d2;
+
+            ValueSet response = new ValueSet();
+            response.Add("RESULT", result);
+            await args.Request.SendResponseAsync(response);
+
+            // log the request in the UI for demo purposes
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                //tbRequests.Text += string.Format("Request: {0} + {1} --> Response = {2}\r\n", d1, d2, result);
+            });
+        }
+
+        /// <summary>
+        /// Ask user if they want to reconnect to the desktop process
+        /// </summary>
+        private async void Reconnect()
+        {
+            //if (App.IsForeground)
+            //{
+            //    MessageDialog dlg = new MessageDialog("Connection to desktop process lost. Reconnect?");
+            //    UICommand yesCommand = new UICommand("Yes", async (r) =>
+            //    {
+            //        await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            //    });
+            //    dlg.Commands.Add(yesCommand);
+            //    UICommand noCommand = new UICommand("No", (r) => { });
+            //    dlg.Commands.Add(noCommand);
+            //    await dlg.ShowAsync();
+            //}
+        }
+
+        private async void Test_Click(object sender, RoutedEventArgs e)
+        {
+            ValueSet request = new ValueSet();
+            request.Add("KEY", "Muthu");
+            AppServiceResponse response = await App.Connection.SendMessageAsync(request);
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AppServiceConnection connection = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeAppServiceConnection();
+        }
+
+        /// <summary>
+        /// Open connection to UWP app service
+        /// </summary>
+        private async void InitializeAppServiceConnection()
+        {
+            connection = new AppServiceConnection();
+            connection.AppServiceName = "SampleInteropService";
+            connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            connection.RequestReceived += Connection_RequestReceived;
+            connection.ServiceClosed += Connection_ServiceClosed;
+
+            AppServiceConnectionStatus status = connection.OpenAsync().GetResults();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                // something went wrong ...
+                MessageBox.Show(status.ToString());
+                this.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Handles the event when the app service connection is closed
+        /// </summary>
+        private void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            // connection to the UWP lost, so we shut down the desktop process
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                Application.Current.Shutdown();
+            }));
+        }
+
+        /// <summary>
+        /// Handles the event when the desktop process receives a request from the UWP app
+        /// </summary>
+        private async void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // retrive the reg key name from the ValueSet in the request
+            string key = args.Request.Message["KEY"] as string;
+            int index = key.IndexOf('\\');
+            if (index > 0)
+            {
+                // read the key values from the respective hive in the registry
+                string hiveName = key.Substring(0, key.IndexOf('\\'));
+                string keyName = key.Substring(key.IndexOf('\\') + 1);
+                RegistryHive hive = RegistryHive.ClassesRoot;
+
+                switch (hiveName)
+                {
+                    case "HKLM":
+                        hive = RegistryHive.LocalMachine;
+                        break;
+                    case "HKCU":
+                        hive = RegistryHive.CurrentUser;
+                        break;
+                    case "HKCR":
+                        hive = RegistryHive.ClassesRoot;
+                        break;
+                    case "HKU":
+                        hive = RegistryHive.Users;
+                        break;
+                    case "HKCC":
+                        hive = RegistryHive.CurrentConfig;
+                        break;
+                }
+
+                using (RegistryKey regKey = RegistryKey.OpenRemoteBaseKey(hive, "").OpenSubKey(keyName))
+                {
+                    // compose the response as ValueSet
+                    ValueSet response = new ValueSet();
+                    if (regKey != null)
+                    {
+                        foreach (string valueName in regKey.GetValueNames())
+                        {
+                            response.Add(valueName, regKey.GetValue(valueName).ToString());
+                        }
+                    }
+                    else
+                    {
+                        response.Add("ERROR", "KEY NOT FOUND");
+                    }
+                    // send the response back to the UWP
+                    args.Request.SendResponseAsync(response);
+                }
+            }
+            else
+            {
+                ValueSet response = new ValueSet();
+                response.Add("ERROR", "INVALID REQUEST");
+                args.Request.SendResponseAsync(response);
+            }
+        }
+    }
+}
